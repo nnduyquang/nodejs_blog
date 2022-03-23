6a4ddb0be41e395e0e0f153a8b854b31e290a7a2
Nguyen Quang <nguyenquang@Nguyens-MacBook-Pro.local>
2022-03-23 17:44:41 +0700

    Template engine

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path=require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', handlebars.engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+//Template engine-config path folder resources
+app.set('views',path.join(__dirname,'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/tin-tuc', (req, res) => {
+  res.render('tin-tuc')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
